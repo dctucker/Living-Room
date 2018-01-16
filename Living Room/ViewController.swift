05898c99e6ac256d2c18a0f7dc98fb5027b0c03f
D. Casey Tucker <dcaseytucker@gmail.com>
2018-01-16 15:33:39 -0800

    Initial Commit

---
+++ Living Room/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Living Room
+//
+//  Created by Douglas Casey Tucker on 1/16/18.
+//  Copyright © 2018 Douglas Casey Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
